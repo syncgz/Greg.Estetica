aee0cac0f0c88772c0bb50952e14fcf752cd64c4
greg_notebook <sync.gz@gmail.com>
2012-10-27 18:40:44 +0200

    IoC container + init utest

---
+++ Greg.Estetica.IoC.Test/ContainerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Greg.Estetica.IoC.Test
+{
+    [TestFixture]
+    public class ContainerTest
+    {
+        [Test]
+        public void Foo()
+        {
+            
+        }
+    }
+}
---
+++ Greg.Estetica.Ioc/Container.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Greg.Estetica.Core.Interfaces;
+using Greg.Estetica.Core.Model;
+using Moq;
+using Ninject;
+
+namespace Greg.Estetica.Ioc
+{
+    public class Container
+    {
+        private static IKernel _kernel = new StandardKernel();
+
+        public static T ResolveType<T>()
+        {
+            if(_kernel == null)
+            {
+                ContainerInitialization();
+            }
+
+            return _kernel.Get<T>();
+        }
+
+        private static void ContainerInitialization()
+        {
+            _kernel = new StandardKernel();
+
+            CompositionRoot();
+        }
+
+        private static void CompositionRoot()
+        {
+            Mock<IPromotionRepository> mock = new Mock<IPromotionRepository>();
+
+            mock.Setup(x => x.GetPromotionList()).Returns(
+                new List<PromotionItem>()
+                    {
+                            new PromotionItem()
+                                {
+                                    Description = "Promocja na paznokcie.", 
+                                    ImagePath = "images/picture4.gif", 
+                                    Link = new Uri("http://www.wp.pl"), 
+                                    Title = "Title"
+                                },
+                            new PromotionItem()
+                            {Description = "Promocja na zele", ImagePath = "images/picture4.gif", Link = new Uri("http://www.wp.pl"), Title = "Title"},
+                            new PromotionItem()
+                            {Description = "Uruchomienie nowej strony internetowej.", ImagePath = "images/picture4.gif", Link = new Uri("http://www.wp.pl"), Title = "Title"}
+                    });
+
+            _kernel.Bind<IPromotionRepository>().ToConstant(mock.Object);
+        }
+    }
+}
