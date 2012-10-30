884b56695beee5a5fe22296db7ab8ee132eea6be
syncgz <sync.gz@gmail.com>
2012-10-30 16:19:28 +0100

    Commit

---
+++ Greg.Estetica/Scripts/Custom/MenuItemSelection.js
@@ -0,0 +1,31 @@
+﻿
+var menuSelector = 'menu_item';
+
+function SelectItem(item) 
+{
+    $('menu>ul>li>a').removeData(menuSelector);
+
+    $(item).data(menuSelector, '1');
+
+    UpdateMenu();
+
+}
+
+function UpdateMenu() 
+{
+    $('menu>ul>li>a').removeClass().each(function () 
+    {
+        if ($(this).data(menuSelector) == '1') 
+        {
+            $(this).addClass('test');
+        }
+    });
+}
+
+function RegisterMenuEvents() 
+{
+    $('menu>ul>li>a').click(function () 
+    {
+        SelectItem($(this));
+    });
+}
