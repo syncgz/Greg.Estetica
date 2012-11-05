61ae001b15dddfe6af9f2883957633b684dac9c1
syncgz <sync.gz@gmail.com>
2012-11-05 11:15:53 +0100

    Gallery with popup added

--- Greg.Estetica/Scripts/Custom/MenuAjax.js
+++ Greg.Estetica/Scripts/Custom/MenuAjax.js
@@ -4,15 +4,16 @@
     
     $('#menu>ul>li>a').click(function (event)
     {
-        
-        
         event.preventDefault();
         
         var url = $(this).attr('href');
         
-        $('#ajaxBlock').load(url);
+        $('#ajaxBlock').load(url, function ()
+        {
+            GalleryMenu();
+        });
         
-        GalleryMenu();
+        
         
     });
     
@@ -21,22 +22,34 @@
 
 function GalleryMenu(parameters)
 {
-    var abc = $(".galleryItem").length;
-    
-    alert("Gallery menu" + abc);
-
-    $(".galleryItem").click(function (e)
+    $("img.galleryItem").bind('click', function (e)
     {
-        alert("....");
         ShowPhotoDialog();
-        
     });
 }
 
 function ShowPhotoDialog() {
+    
+    
+
     $("#dialog-modal").dialog({
-        height: 140,
-        modal: true
+        height: 'auto',
+        width:'auto',
+        dialogClass: "foo"
     });
+    
+    $(".foo .ui-dialog-titlebar").css("display", "none");
+    $(".foo .ui-dialog-title").css("font-size", "5px");
+    $(".foo .ui-widget-header").css("display", "none");
+    $(".foo .ui-widget-content").css("background-color", "black");
+    $(".foo .ui-widget-content").css("font-size", "12px");
+    $(".ui-corner-all").css("background-color", "black");
+    $(".ui-resizable-n").css("background-image", "none");
+    
+    //$("#dialog-modal").position({
+    //    my: "center",
+    //    at: "center",
+    //    of: window
+    //});
 };
 
